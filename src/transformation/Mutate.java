902c910d1e96cc19c765325b21b21b8e65f723fd
jia.liu <jia.liu@ctrip.com>
2015-05-11 11:21:33 +0800

    test jinjava

--- src/transformation/Mutate.java
+++ src/transformation/Mutate.java
@@ -2,7 +2,12 @@ package transformation;
 
 import org.apache.spark.api.java.function.Function;
 
+import com.hubspot.jinjava.Jinjava;
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import com.hubspot.jinjava.tree.Node;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class Mutate implements Function {
 	@Override
@@ -11,5 +16,22 @@ public class Mutate implements Function {
 		HashMap<String, Object> event = (HashMap<String, Object>) arg0;
 
 		return event;
+	}
+	
+	public static void main(String[] args) {
+		long s = System.currentTimeMillis();
+		
+		Jinjava jinjava = new Jinjava();
+
+		Map<String, Object> context = new HashMap<>();      
+		context.put("name", "Jared");
+		String template = "Hello, {% if name is defined %} {{name}} {% else %} world {% endif %}";
+
+		for (int i = 0; i < 70000; i++) {
+
+		    jinjava.renderForResult(template, context);
+
+		}       
+		System.out.println(System.currentTimeMillis()-s);
 	}
 }
