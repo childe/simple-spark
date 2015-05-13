3d0e577f81b03f181a2ec8f807b9625328ed6061
jia.liu <jia.liu@ctrip.com>
2015-05-13 19:06:12 +0800

    new file:   src/jinfilter/Floor.java

---
+++ src/jinfilter/Floor.java
@@ -0,0 +1,80 @@
+package jinfilter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.hubspot.jinjava.Jinjava;
+import com.hubspot.jinjava.interpret.Context;
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import com.hubspot.jinjava.lib.filter.Filter;
+import com.hubspot.jinjava.parse.TokenParser;
+import com.hubspot.jinjava.tree.Node;
+import com.hubspot.jinjava.tree.TreeParser;
+
+public class Floor implements Filter {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "floor";
+	}
+
+	@Override
+	public Object filter(Object arg0, JinjavaInterpreter arg1, String... arg2) {
+		// TODO Auto-generated method stub
+		int a = (Integer) arg0;
+		Object pre = null;
+		int current;
+		for (int i = 0; i < arg2.length; i++) {
+			current = Integer.parseInt(arg2[i]);
+			if (a <= current) {
+				return pre;
+			}
+			pre = current;
+		}
+		return pre;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Floor f = new Floor();
+
+		// Object a;
+		// a = f.filter(10, null, "5", "7", "20");
+		// System.out.println(a);
+		//
+		// a = f.filter(1, null, "5", "7", "20");
+		// System.out.println(a);
+		//
+		// a = f.filter(100, null, "5", "7", "20");
+		// System.out.println(a);
+
+		Map<String, Object> bindings = new HashMap<>();
+		bindings.put("name", "Jared");
+		bindings.put("age", 13);
+
+		Jinjava jinjava = new Jinjava();
+
+		Context c = jinjava.getGlobalContext();
+		c.registerFilter(f);
+		String template = "Hello, {%if name is defined %} {{name}} {% endif %}, age is{{ age - age%10 }}";
+//		TokenParser t = new TokenParser(null, template);
+//		Node parsedTemplate = TreeParser.parseTree(t);
+
+		long s = System.currentTimeMillis();
+		for (int i = 0; i < 1; i++) {
+			Context cc = new Context(c, bindings);
+			JinjavaInterpreter interpreter = new JinjavaInterpreter(jinjava,
+					cc, null);
+			// Node parsedTemplate = interpreter.parse(template);
+			String ss = interpreter.render(template);
+			System.out.println(ss);
+			
+			
+		}
+
+		System.out.println(System.currentTimeMillis() - s);
+	}
+}
