967ba1f7ffa9abe764fbc4bdcc9307b3126699fb
jia.liu <jia.liu@ctrip.com>
2015-05-13 13:16:21 +0800

    add some tests

--- src/function/Mutate.java
+++ src/function/Mutate.java
@@ -6,13 +6,16 @@ import com.hubspot.jinjava.Jinjava;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 import com.hubspot.jinjava.tree.Node;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Mutate implements Function {
-	
+
 	static public final String defaultTransformation = "map";
-	
+
 	@Override
 	public Object call(Object arg0) throws Exception {
 		// TODO Auto-generated method stub
@@ -20,21 +23,89 @@ public class Mutate implements Function {
 
 		return event;
 	}
+
+	public void testFunc1(String a, int b) {
+		for (int i = 0; i < b; i++) {
+			System.out.println(a);
+		}
+	}
 	
+	public void testFunc2() {
+		System.out.println("testFunc2");
+	}
+
 	public static void main(String[] args) {
-		long s = System.currentTimeMillis();
+
+		// test getField
+
+		String a = null;
+		try {
+			a = (String) Class.forName("function.Mutate")
+					.getField("defaultTransformation").get(null);
+		} catch (IllegalArgumentException | IllegalAccessException
+				| NoSuchFieldException | SecurityException
+				| ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println(a);
+
+		// test getMethod with uncertain parameters
+
+		ArrayList<Class> parameters = new ArrayList<Class>();
+		Class[] p = {};
+		parameters.add(String.class);
+		parameters.add(int.class);
+		 p =parameters.toArray(p);
+		System.out.println(p);
 		
+		Class[] p2 = new Class[] { String.class, int.class };
+		System.out.println(p2);
+		
+		try {
+			Method m = Class.forName("function.Mutate").getMethod("testFunc1",
+					p);
+
+			m.invoke(Class.forName("function.Mutate").newInstance(), "ab", 10);
+
+		} catch (IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | InstantiationException
+				| NoSuchMethodException | SecurityException
+				| ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// test getMethod with void parameter
+		
+		try {
+			p = new Class[]{};
+			Method m = Class.forName("function.Mutate").getMethod("testFunc2",
+					p);
+
+			m.invoke(Class.forName("function.Mutate").newInstance());
+
+		} catch (IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | InstantiationException
+				| NoSuchMethodException | SecurityException
+				| ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		long s = System.currentTimeMillis();
+
 		Jinjava jinjava = new Jinjava();
 
-		Map<String, Object> context = new HashMap<>();      
+		Map<String, Object> context = new HashMap<>();
 		context.put("name", "Jared");
 		String template = "Hello, {% if name is defined %} {{name}} {% else %} world {% endif %}";
 
 		for (int i = 0; i < 70000; i++) {
 
-		    jinjava.renderForResult(template, context);
+			jinjava.renderForResult(template, context);
 
-		}       
-		System.out.println(System.currentTimeMillis()-s);
+		}
+		System.out.println(System.currentTimeMillis() - s);
 	}
 }
