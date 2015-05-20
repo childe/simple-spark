2bf3f49aa2553dccfa76b12feefb01b4b75ed6d8
jia.liu <jia.liu@ctrip.com>
2015-05-21 02:25:35 +0800

    convert to long

--- src/jinfilter/IntegerFilter.java
+++ src/jinfilter/IntegerFilter.java
@@ -7,13 +7,9 @@ import java.util.Map;
 
 import jinmanager.JinManager;
 
-import com.hubspot.jinjava.Jinjava;
 import com.hubspot.jinjava.interpret.Context;
 import com.hubspot.jinjava.interpret.JinjavaInterpreter;
 import com.hubspot.jinjava.lib.filter.Filter;
-import com.hubspot.jinjava.parse.TokenParser;
-import com.hubspot.jinjava.tree.Node;
-import com.hubspot.jinjava.tree.TreeParser;
 
 public class IntegerFilter implements Filter {
 
@@ -26,12 +22,26 @@ public class IntegerFilter implements Filter {
 	@Override
 	public Object filter(Object arg0, JinjavaInterpreter arg1, String... arg2) {
 		// TODO Auto-generated method stub
-		try {
-			return Integer.parseInt((String) arg0);
-		} catch (Exception e) {
+		Class c = arg0.getClass();
+		if (c == String.class) {
 			try {
-				return (int)Double.parseDouble((String) arg0);
-			} catch (Exception e2) {
+				return Long.parseLong((String) arg0);
+			} catch (Exception e) {
+				try {
+					return (long) Double.parseDouble((String) arg0);
+				} catch (Exception e2) {
+					return 0;
+				}
+			}
+		} else if (c == Double.class) {
+			return ((Double) arg0).longValue();
+		} else if (c == Float.class) {
+			return ((Float) arg0).longValue();
+
+		} else {
+			try {
+				return (long) arg0;
+			} catch (Exception e) {
 				return 0;
 			}
 		}
@@ -41,13 +51,15 @@ public class IntegerFilter implements Filter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		Float d = 1.1f;
+		long a = d.longValue();
 		final Map<String, Object> value = new HashMap<String, Object>() {
 			{
 
 				put("name", "jia.liu");
 				put("age", 29);
 				put("logtime", "1432020638.958");
+				put("longtime", 1432144740100.0d);
 
 			}
 		};
@@ -68,25 +80,28 @@ public class IntegerFilter implements Filter {
 		Context cc = new Context(JinManager.c, binding);
 		String template, s;
 
+		template = "{{(event[1].longtime|integer/1000)|integer}}";
+		s = JinManager.jinjava.render(template, cc);
+		System.out.println(s);
 
-		long st = System.currentTimeMillis();
-
-		for (int i = 0; i < 10000; i++) {
-
-			template = "{{event[1].logtime|double|int}}";
-			s = JinManager.jinjava.render(template, cc);
-			// System.out.println(s);
-		}
-		System.out.println(System.currentTimeMillis() - st);
+		 long st = System.currentTimeMillis();
 		
-		st = System.currentTimeMillis();
-
-		for (int i = 0; i < 10000; i++) {
-
-			template = "{{event[1].logtime|integer}}";
-			s = JinManager.jinjava.render(template, cc);
-			// System.out.println(s);
-		}
-		System.out.println(System.currentTimeMillis() - st);
+		 for (int i = 0; i < 10000; i++) {
+		
+		 template = "{{event[1].logtime|double|int}}";
+		 s = JinManager.jinjava.render(template, cc);
+		 // System.out.println(s);
+		 }
+		 System.out.println(System.currentTimeMillis() - st);
+		
+		 st = System.currentTimeMillis();
+		
+		 for (int i = 0; i < 10000; i++) {
+		
+		 template = "{{event[1].logtime|integer}}";
+		 s = JinManager.jinjava.render(template, cc);
+		 // System.out.println(s);
+		 }
+		 System.out.println(System.currentTimeMillis() - st);
 	}
 }
