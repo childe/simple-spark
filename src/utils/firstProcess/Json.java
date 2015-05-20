9056a3f5e037fdf30634a5315e56c12ac61d5672
jia.liu <jia.liu@ctrip.com>
2015-05-20 18:44:22 +0800

    if message is null

--- src/utils/firstProcess/Json.java
+++ src/utils/firstProcess/Json.java
@@ -17,6 +17,7 @@ public class Json implements PairFunction {
 		Tuple2 t = (Tuple2) arg0;
 		Object key = t._1;
 		final String message = (String) t._2;
+		
 
 		HashMap<String, Object> event = null;
 		try {
@@ -25,6 +26,14 @@ public class Json implements PairFunction {
 			event = new HashMap<String, Object>() {
 				{
 					put("message", message);
+				}
+			};
+		}
+		
+		if (event == null) {
+			event = new HashMap<String, Object>() {
+				{
+					put("message", "");
 				}
 			};
 		}
